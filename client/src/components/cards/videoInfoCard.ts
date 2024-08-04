0e98626a9eddac9bce82027d7a72b47ee9acfadf
Birendra Bohara <boharabirendra546@gmail.com>
2024-08-04 15:04:58 +0545

    Add share button

--- client/src/components/cards/videoInfoCard.ts
+++ client/src/components/cards/videoInfoCard.ts
@@ -35,6 +35,15 @@ export const VideoInfoCard = (video: IVideo) => `
                 ${mustLoginMessageCard()}
               </div>
             </div>
+            <div>
+               <button id="share-btn" title="Copy link" class="flex items-center px-6 py-3 bg-bglikebtn rounded-full transition duration-300 gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"  stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                  <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+                </svg>
+                <span id="share-msg">Copy link</span>
+                </button>
+            </div>
           </div>
          ${VideoDescriptionCard(video.description)}
         </div>
